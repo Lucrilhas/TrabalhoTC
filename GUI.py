676704bc74efdc3f163c92570d3c760bafcd4398
Lucas Cruvinel <LucrilhasBR@hotmail.com>
2021-11-21 16:04:18 -0500

    5Tupla

--- GUI.py
+++ GUI.py
@@ -8,22 +8,23 @@ from Textos import txts
 TAMANHO_PROG = (1000, 800)
 AZUL_CLARO = '#ADD8E6'
 
+
 # Classe de inteface
 class Programa:
     def __init__(self):
         # Variaveis de configuração
-
+        self.dados = None
         # Variaveis de interface
         janela = tk.Tk()  # Janela mais profunda
-        janela.title('Automatos de Pilha')         # Nome da janela
-        janela.geometry(f'{TAMANHO_PROG[0]}x{TAMANHO_PROG[1]}')   # Tamanho da janela
-        janela.iconbitmap('imgs/icone_janela.ico')    # Icone
+        janela.title('Automatos de Pilha')  # Nome da janela
+        janela.geometry(f'{TAMANHO_PROG[0]}x{TAMANHO_PROG[1]}')  # Tamanho da janela
+        janela.iconbitmap('imgs/icone_janela.ico')  # Icone
         janela['bg'] = AZUL_CLARO
 
         # Divide a janela em header e Body
         header = tk.Frame(janela, height=100)
         header.pack(fill='x', side='top')
-        div = tk.Frame(janela, bg='black', height=5)    # Linha de divisão entre
+        div = tk.Frame(janela, bg='black', height=5)  # Linha de divisão entre
         div.pack(fill='x', side='top')
         body = tk.Frame(janela, bg=AZUL_CLARO)
         body.pack(fill='both', expand=True, side='top')
@@ -44,19 +45,73 @@ class Programa:
         # Guia do Deterministico
 
         # Explicação da quintupla
+        frm_afd = tk.LabelFrame(tab[0], text='O que é um Autômato Deterministico?', font=("Times new Roman", 15))
+        tk.Label(frm_afd, text=txts['AFD'], justify='left', font=("Times new Roman", 15)).grid(column=0, row=0,
+                                                                                               sticky='W')
+        frm_afd.grid(column=0, row=0, columnspan=2, sticky='news', padx=(10, 10), pady=(10, 10))
+
+        # Explicação da quintupla
         frm_5Tupla_explicacao = tk.LabelFrame(tab[0], text='Definição de uma 5-Tupla:', font=("Times new Roman", 15))
-        tk.Label(frm_5Tupla_explicacao, text= txts['Exp5Tupla'], justify='left', font=("Times new Roman", 15)).grid(column=0, row=0, sticky='W')
-        frm_5Tupla_explicacao.grid(column=0, row=0, sticky='W', padx=(10,10), pady=(10,10))
+        tk.Label(frm_5Tupla_explicacao, text=txts['Exp5Tupla'], justify='left', font=("Times new Roman", 15)).grid(
+            column=0, row=0, sticky='W')
+        frm_5Tupla_explicacao.grid(column=0, row=1, sticky='news', padx=(10, 10), pady=(10, 10))
 
         # Coleta dados da quintupla
-        edits_5Tupla = []
+        self.edits_5Tupla = []  # 0-Conjunto de estados, 1-Alfabeto de entradas, 2-Estado inicial, 3-Estado final, 4-Regras
         frm_5Tupla_coleta = tk.LabelFrame(tab[0], text='Valores da sua 5-Tupla:', font=("Times new Roman", 15))
         for indc, txt in enumerate(txts['Col5Tupla']):
-            edits_5Tupla.append(tk.Entry(frm_5Tupla_coleta, bd=2, width=50))
-            tk.Label(frm_5Tupla_coleta, text=txt, justify='left', font=("Times new Roman", 15)).grid( column=0, row=indc, sticky='W')
-            edits_5Tupla[indc].grid( column=1, row=indc, sticky='W', padx=(5, 5))
-        frm_5Tupla_coleta.grid(column=1, row=0, sticky='W', padx=(10, 10), pady=(10, 10))
+            self.edits_5Tupla.append(tk.Entry(frm_5Tupla_coleta, bd=2, width=50))
+            tk.Label(frm_5Tupla_coleta, text=txt, justify='left', font=("Times new Roman", 15)).grid(column=0, row=indc,
+                                                                                                     sticky='W')
+            self.edits_5Tupla[indc].grid(column=1, row=indc, sticky='W', padx=(5, 5))
+        frm_5Tupla_coleta.grid(column=0, row=2, sticky='news', padx=(10, 10), pady=(10, 10))
 
+        # Botao inicia
+        btn_start = tk.Button(frm_5Tupla_coleta, text="Iniciar", width=10, height=3, command=self.inicia_prog)
+        btn_start.grid(column=1, row=4, sticky='ne', padx=(5, 5))
+        btn_start = tk.Button(frm_5Tupla_coleta, text="teste", width=10, height=3, command=self.teste_insert)
+        btn_start.grid(column=0, row=4, sticky='ne', padx=(5, 5))
+
+        # Coleta regra de transicao
+        frm_regras_trans = tk.LabelFrame(tab[0], text='Regras de Transição da sua 5-Tupla:',
+                                         font=("Times new Roman", 15))
+        self.edits_5Tupla.append(tk.Text(frm_regras_trans, bd=2, width=50))
+        scrl_5Tupla = tk.Scrollbar(frm_regras_trans)
+        self.edits_5Tupla[4].configure(yscrollcommand=scrl_5Tupla.set)
+        scrl_5Tupla.config(command=self.edits_5Tupla[4].yview)
+        self.edits_5Tupla[4].pack(side=tk.LEFT)
+        scrl_5Tupla.pack(side=tk.RIGHT, fill=tk.Y)
+        frm_regras_trans.grid(column=1, row=1, rowspan=2, padx=(10, 10), pady=(10, 10))
 
         # Inicializacao
         janela.mainloop()
+
+    def teste_insert(self):
+        self.edits_5Tupla[0].insert(0, 'q0, q1, q2, qf')
+        self.edits_5Tupla[1].insert(0, '0, 1')
+        self.edits_5Tupla[2].insert(0, 'q0')
+        self.edits_5Tupla[3].insert(0, 'qf')
+        self.edits_5Tupla[4].insert('1.0', 'q0, 0, q1\nq1, 1, q2\nq2, 1, q1\nq2, 0, q2\n')
+
+    def inicia_prog(self):
+        self.dados = {
+            'Q': self.edits_5Tupla[0].get(),
+            'E': self.edits_5Tupla[1].get(),
+            'q': self.edits_5Tupla[2].get(),
+            'F': self.edits_5Tupla[3].get(),
+            'FT': self.edits_5Tupla[4].get('1.0', tk.END)
+        }
+        print('Pré tratamento:', self.dados)
+        self.tratamento_dados()
+
+    def tratamento_dados(self):
+        self.dados = {
+            'Q': self.dados['Q'].replace(',', '').split(),
+            'E': self.dados['E'].replace(',', '').split(),
+            'q': self.dados['q'].replace(',', '').split(),
+            'F': self.dados['F'].replace(',', '').split(),
+            'FT': [elem.replace(',', '').split() for elem in self.dados['FT'].split('\n') if elem != '']
+        }
+
+
+        print('Pós tratamento:', self.dados)
